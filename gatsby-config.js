40fa8651d55d4de36c96b3e21d20f1710d384d7d
karolis <karolis.stulgys@gmail.com>
2019-11-05 21:05:08 +1100

    add yml content

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     "gatsby-transformer-remark",
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-yaml`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
